f3128f23033d8e39cc3c230d0ceb2848307bef25
SHINSUKE ARAGAKI <32014881+shinsuke-Alex@users.noreply.github.com>
2019-01-13 17:09:52 +0900

    Create Disemvowel_Trolls.rb

---
+++ Disemvowel_Trolls.rb
@@ -0,0 +1,23 @@
+'''
+Description:
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+'''
+
+# My Solution
+
+def disemvowel(str)
+  str.delete('aeiouAEIOU')
+end
+
+
+# Test Cases:
+Test.assert_equals(disemvowel("This website is for losers LOL!"),
+  "Ths wbst s fr lsrs LL!")
