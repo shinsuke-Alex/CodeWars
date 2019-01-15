66de411ec07a73581e71af820b4b66b3133d080f
SHINSUKE ARAGAKI <32014881+shinsuke-Alex@users.noreply.github.com>
2019-01-15 22:12:53 +0900

    Update Credit_Card_Mask.rb

--- Credit_Card_Mask.rb
+++ Credit_Card_Mask.rb
@@ -1,21 +1,20 @@
-'''
+
 Description:
 Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
 Your task is to write a function maskify, which changes all but the last four characters into '#'.
-
 Examples
 maskify('4556364607935616') # should return '############5616'
 maskify('64607935616')      # should return '#######5616'
-'''
 
-# My Solution
+
+#　My Solution
 
 def maskify(cc)
   cc.gsub(/.(?=.{4})/, "#")
 end
 
 
-#logic
+#　logic
  　#文字列の中で正規表現のパターンにマッチした部分をすべて指定の文字列に置換。（４文字を「#」に置換）
  　#第1引数に正規表現のパターン（pattern）、第2引数に文字列（replacement）を指定
   
